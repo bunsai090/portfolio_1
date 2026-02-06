1d8fb7cfc1a267797b986960afb17530350e8424
bunsai <bunsai090saporno@gmail.com>
2026-02-06 19:32:40 +0800

    Update interestsData.js

--- src/components/AboutMe/interestsData.js
+++ src/components/AboutMe/interestsData.js
@@ -1,14 +1,14 @@
 // Import Music Covers
 import kalapastanganCover from '../../assets/kalapastangan.png';
 import balisongCover from '../../assets/balisong .png';
-import creepCover from '../../assets/creep .png';
+import isipCover from '../../assets/isip.jpg';
 import mundoCover from '../../assets/mundo.png';
 import panaginipCover from '../../assets/panaginip.png';
 import sagipCover from '../../assets/sagip.png';
 import medisinaCover from '../../assets/Medisina .png';
 import sesameCover from '../../assets/Sesame Syrup.png';
 import secretdoorCover from '../../assets/secret door.png';
-import allthestarsCover from '../../assets/All The Stars.png';
+
 import meanItCover from '../../assets/mean it.png';
 
 // Import Movie Covers
@@ -55,14 +55,14 @@ const interestsData = {
         items: [
             { id: 1, title: "Kalapastangan", artist: "fitterkarma", album: "Kalapastangan", date: "Sep 22, 2025", duration: "3:45", cover: kalapastanganCover },
             { id: 2, title: "Balisong", artist: "Rico Blanco", album: "Your Universe", date: "Sep 21, 2025", duration: "3:58", cover: balisongCover },
-            { id: 3, title: "Creep", artist: "Radiohead", album: "Pablo Honey", date: "Sep 20, 2025", duration: "3:56", cover: creepCover },
+            { id: 3, title: "Isip", artist: "Healy After Dark", album: "Isip", date: "Sep 20, 2025", duration: "4:12", cover: isipCover },
             { id: 4, title: "Mundo", artist: "IV of Spades", album: "ClapClapClap!", date: "Sep 19, 2025", duration: "5:50", cover: mundoCover },
             { id: 5, title: "Panaginip", artist: "nicole", album: "Panaginip", date: "Sep 18, 2025", duration: "4:20", cover: panaginipCover },
             { id: 6, title: "Sagip", artist: "Jan Roberts", album: "Sagip", date: "Sep 17, 2025", duration: "3:52", cover: sagipCover },
             { id: 7, title: "Medisina", artist: "Zild", album: "Medisina", date: "Sep 16, 2025", duration: "4:05", cover: medisinaCover },
             { id: 8, title: "Sesame Syrup", artist: "Cigarettes After Sex", album: "Cigarettes After Sex", date: "Sep 15, 2025", duration: "4:48", cover: sesameCover },
             { id: 9, title: "Mean It", artist: "Lauv & LANY", album: "~how i'm feeling~", date: "Sep 14, 2025", duration: "3:53", cover: meanItCover },
-            { id: 10, title: "All The Stars", artist: "Kendrick Lamar, SZA", album: "Black Panther", date: "Sep 13, 2025", duration: "3:52", cover: allthestarsCover }
+            { id: 10, title: "Secret Door", artist: "Arctic Monkeys", album: "Humbug", date: "Sep 13, 2025", duration: "3:43", cover: secretdoorCover }
         ]
     },
     movies: {
